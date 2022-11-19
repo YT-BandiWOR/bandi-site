76d60acd9df41257e794b75f91c18bbd31975719
YT-BandiWOR <kira.morozovkirill.2018.aprel@gmail.com>
2022-11-19 16:28:10 +0300

    base structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from "../App.module.scss.module.scss";
+
+
+const Footer = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "../App.module.scss.module.scss";
+
+const Header = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
